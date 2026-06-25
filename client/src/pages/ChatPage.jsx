4091dc483628cf4ebe79d84d207076fdab81b147
Shravanii89 <adhavshravani6@gmail.com>
2026-06-25 12:07:54 +0530

    Added responsive mobile sidebar

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -46,6 +46,9 @@ function ChatPage() {
     return localStorage.getItem("darkMode") === "true";
   });
 
+  const [sidebarOpen, setSidebarOpen] =
+    useState(false);
+
   useEffect(() => {
     localStorage.setItem(
       "allChats",
@@ -204,28 +207,69 @@ function ChatPage() {
           : "bg-gray-100 text-black"
       }`}
     >
-      <Sidebar
-  chats={chats}
-  currentChat={currentChat}
-  onNewChat={createNewChat}
-  onSelectChat={selectChat}
-  onDeleteChat={deleteChat}
-  onRenameChat={(id, title) => {
-    setChats((prev) =>
-      prev.map((chat) =>
-        chat.id === id
-          ? { ...chat, title }
-          : chat
-      )
-    );
-  }}
-/>
+      {sidebarOpen && (
+        <div
+          className="fixed inset-0 bg-black/50 z-40 md:hidden"
+          onClick={() =>
+            setSidebarOpen(false)
+          }
+        />
+      )}
+
+      <div
+        className={`
+          fixed md:static z-50 h-screen
+          transform transition-transform duration-300
+          ${
+            sidebarOpen
+              ? "translate-x-0"
+              : "-translate-x-full"
+          }
+          md:translate-x-0
+        `}
+      >
+        <Sidebar
+          chats={chats}
+          currentChat={currentChat}
+          onNewChat={createNewChat}
+          onSelectChat={(id) => {
+            selectChat(id);
+            setSidebarOpen(false);
+          }}
+          onDeleteChat={deleteChat}
+          onRenameChat={(id, title) => {
+            setChats((prev) =>
+              prev.map((chat) =>
+                chat.id === id
+                  ? {
+                      ...chat,
+                      title,
+                    }
+                  : chat
+              )
+            );
+          }}
+        />
+      </div>
 
       <div className="flex-1 min-h-screen">
-        <Navbar
-          darkMode={darkMode}
-          setDarkMode={setDarkMode}
-        />
+        <div className="flex items-center">
+          <button
+            onClick={() =>
+              setSidebarOpen(true)
+            }
+            className="md:hidden text-2xl px-4"
+          >
+            ☰
+          </button>
+
+          <div className="flex-1">
+            <Navbar
+              darkMode={darkMode}
+              setDarkMode={setDarkMode}
+            />
+          </div>
+        </div>
 
         <div className="max-w-4xl mx-auto p-4">
           <button
